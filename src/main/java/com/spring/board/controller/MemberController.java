17f1be3d0a80015c8320f711513a93598dc6c282
parkyejin <uiop5809@naver.com>
2024-07-25 19:44:44 +0900

    feat: Member Logout, Login check

--- src/main/java/com/spring/board/controller/MemberController.java
+++ src/main/java/com/spring/board/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.spring.board.repository.MemberRepository;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,6 +41,50 @@ public class MemberController {
             }
         } else {
             result.put("message", "업거나 삭제된 id");
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    // 로그아웃 기능
+    @GetMapping("/member/logout")
+    @ResponseBody
+    public Map<String, Object> logout(HttpSession session) {
+        HashMap<String, Object> result = new HashMap<>();
+
+        Object object = session.getAttribute("member");
+        String message = "";
+        if(object == null) {
+            message += "로그인 안 된 상태";
+        } else {
+            message += "로그인 된 상태";
+        }
+
+        // 브라우저만의 정보를 담은 session 지우고 다시 만듦
+        // 이로써 session에 저장했던 모든 정보를 없애버리니 로그인 안 된 상태로 바꾸는 것
+        session.invalidate(); // 로그인 정보 삭제
+        result.put("code", "ok");
+        result.put("message", message + "로그아웃완료");
+
+        System.out.println(result);
+        return result;
+    }
+
+    // 로그인 체크
+    @GetMapping("/member/check_login")
+    @ResponseBody
+    public Map<String, Object> check_login(HttpSession session) {
+        HashMap<String, Object> result = new HashMap<>();
+
+        Object object = session.getAttribute("member");
+        if (object == null) {
+            result.put("code", "error");
+            result.put("message", "Not Login");
+        } else {
+            result.put("code", "ok");
+            result.put("message", "On Login");
+            Member m = (Member)object;
+            result.put("data", m); // 로그인된 사용자 정보
         }
         System.out.println(result);
         return result;
